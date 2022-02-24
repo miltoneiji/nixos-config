3f6253b96bfa09e62dd5da3c1f9b0e21b1677504
Milton Eiji Takamura <miltontakamura@gmail.com>
2022-02-24 21:01:50 +0000

    !!! System update

--- system/modules/dwm/default.nix
+++ system/modules/dwm/default.nix
@@ -4,6 +4,10 @@
   nixpkgs.overlays = [
     (self: super: {
       dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+        src = builtins.fetchGit {
+          url = "git://git.suckless.org/dwm";
+          rev = "cb3f58ad06993f7ef3a7d8f61468012e2b786cab";
+        };
         patches = [
           ./patches/dwm-fullgaps-6.2.diff
           ./patches/dwm-cool-autostart-6.2.diff
